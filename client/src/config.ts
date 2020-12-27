36dd2dd68470ce08be7bc0ef7c6e3459388b915a
Sebastian <sebastian.jacky@gmail.com>
2020-12-27 16:48:42 +0100

    Fix aws rest api gateway id

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '3ywu178wdl'
+const apiId = '5y9zx3zmc1'
 export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.com/dev`
 
 export const authConfig = {
