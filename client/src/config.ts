8bf252dcfa1b44364b8d3a5959d94fb897568cf6
Sebastian <sebastian.jacky@gmail.com>
2020-12-27 22:07:34 +0100

    Adjust callbackUrl

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,5 +5,5 @@ export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.
 export const authConfig = {
   domain: 'dev-d0xra3u1.eu.auth0.com',            // Auth0 domain
   clientId: '86joCDF1YHM6Htqx7rzxhEppM5lNvbkX',          // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://af2109b1a164d461585753709bfee10f-481974384.eu-central-1.elb.amazonaws.com:3000//callback'
 }
